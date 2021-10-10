cf5af092953ba9eaf8b5d8a67b1a290db3837f4b
18r441m <18r441m@gmail.com>
2021-10-09 20:10:41 -0400

    homework3 solution add

---
+++ packages/homework3/src/talker.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import Float32
+import random
+
+def talker():
+    pub = rospy.Publisher('/homework2/delta', Float32, queue_size=10)
+    rospy.init_node('talker')
+    rate = rospy.Rate(10)
+
+    while not rospy.is_shutdown():
+        pub.publish(random.randint(1, 100))
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
